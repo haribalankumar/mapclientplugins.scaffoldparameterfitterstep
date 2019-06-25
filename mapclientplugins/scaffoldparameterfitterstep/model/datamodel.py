e89bd430cecc9354458a8eb3b57762a90d880795
mahyar-osn <m.osanlouy@auckland.ac.nz>
2019-06-25 20:14:47 +1200

    Data IO is done here instead of master.

--- mapclientplugins/scaffoldparameterfitterstep/model/datamodel.py
+++ mapclientplugins/scaffoldparameterfitterstep/model/datamodel.py
@@ -1,25 +1,44 @@
 from opencmiss.zinc.field import Field
 from opencmiss.zinc.glyph import Glyph
 from opencmiss.zinc.status import OK as ZINC_OK
+from opencmiss.zinc.streamregion import StreaminformationRegion
 
 from ..utils import maths
 
 
+def _read_aligner_description(data_region, data_description):
+    data_stream_information = data_region.createStreaminformationRegion()
+    for key in data_description:
+        if key != 'elements3D' and key != 'elements2D' and key != 'elements1D' and key != 'nodes':
+            if isinstance(key, float):
+                time = key
+            else:
+                time = float(key)
+            memory_resource = data_stream_information.createStreamresourceMemoryBuffer(data_description[key])
+            data_stream_information.setResourceDomainTypes(memory_resource, Field.DOMAIN_TYPE_DATAPOINTS)
+            data_stream_information.setResourceAttributeReal(memory_resource, StreaminformationRegion.ATTRIBUTE_TIME,
+                                                             time)
+    return data_stream_information
+
+
 class DataModel(object):
 
-    def __init__(self, context, region, material_module):
+    def __init__(self, context, region, data_description, material_module):
 
         self._context = context
-        self._region = region
+        self._parent_region = region
+        self._region = self._parent_region.createChild('data_region')
+        self._sir = _read_aligner_description(self._region, data_description)
+
         self._material_module = material_module
         self._scene = None
-        self._data_coordinate_field = None
+        self._coordinate_field = None
 
     def _create_data_point_graphics(self, is_temporal):
         # self._timekeeper.setTime(0.0)
         points = self._scene.createGraphicsPoints()
         points.setFieldDomainType(Field.DOMAIN_TYPE_DATAPOINTS)
-        points.setCoordinateField(self._data_coordinate_field)
+        points.setCoordinateField(self._coordinate_field)
         point_attr = points.getGraphicspointattributes()
         if is_temporal:
             point_size = self._get_auto_point_size()
@@ -35,13 +54,33 @@ class DataModel(object):
     def create_data_graphics(self, is_temporal):
         self._create_data_point_graphics(is_temporal)
 
+    def get_data_coordinate_field(self):
+        fm = self._region.getFieldmodule()
+        data_point_set = fm.findNodesetByFieldDomainType(Field.DOMAIN_TYPE_DATAPOINTS)
+        data_point = data_point_set.createNodeiterator().next()
+        if not data_point.isValid():
+            raise ValueError('Data cloud is empty')
+        cache = fm.createFieldcache()
+        cache.setNode(data_point)
+        field_iter = fm.createFielditerator()
+        field = field_iter.next()
+        while field.isValid():
+            if field.isTypeCoordinate() and (field.getNumberOfComponents() <= 3):
+                if field.isDefinedAtLocation(cache):
+                    return field
+            field = field_iter.next()
+        raise ValueError('Could not determine data coordinate field')
+
     def get_coordinate_field(self):
-        return self._data_coordinate_field
+        return self._coordinate_field
+
+    def get_region(self):
+        return self._region
 
     def _get_data_range(self):
         fm = self._region.getFieldmodule()
         data_points = fm.findNodesetByFieldDomainType(Field.DOMAIN_TYPE_DATAPOINTS)
-        minimums, maximums = self._get_nodeset_minimum_maximum(data_points, self._data_coordinate_field)
+        minimums, maximums = self._get_nodeset_minimum_maximum(data_points, self._coordinate_field)
         return minimums, maximums
 
     def get_range(self):
@@ -73,10 +112,19 @@ class DataModel(object):
         minimums, maximums = self._get_data_range()
         return maths.sub(minimums, maximums)
 
-    def set_coordinate_field(self, field):
-        if self._data_coordinate_field is not None:
-            self._data_coordinate_field = None
-        self._data_coordinate_field = field
+    def initialise_data(self):
+        if self._coordinate_field is not None:
+            self._coordinate_field = None
+        result = self._region.read(self._sir)
+        if result != ZINC_OK:
+            raise ValueError('Failed to read and initialise data cloud.')
+        self._coordinate_field = self.get_data_coordinate_field()
 
-    def set_scene(self, scene):
-        self._scene = scene
+    def initialise_scene(self):
+        self._scene = self._region.getScene()
+
+    def set_coordinate_field(self, field):
+        if self._coordinate_field is not None:
+            self._coordinate_field = None
+        self._coordinate_field = field
+
